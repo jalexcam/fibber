063c9ab0b4021aa7ef26cbdb004b1b0710425979
Alexander Campo <jalexcam@gmail.com>
2026-06-09 01:02:50 -0500

    [1.0] Added new enum

---
+++ src/Container/Enum/Locales.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Fibber Package
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://fibberpackage.com/license
+ *
+ * @package     Fibber
+ * @link        https://fibberpackage.com
+ * @copyright   Copyright (c) 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://fibberpackage.com/license
+ */
+
+namespace Jalexcam\Fibber\Container\Enum;
+
+/**
+ * Allows use a constant for locale as default.
+ */
+class Locales
+{
+    public const DEFAULT = 'default';
+}
