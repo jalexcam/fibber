1bae8469219351ada524303fb7e29ea8691cc66c
Alexander Campo <jalexcam@gmail.com>
2026-06-10 11:32:04 -0500

    Formatting

--- src/Container/ContainerMixin.php
+++ src/Container/ContainerMixin.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Fibber Package
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://fibberpackage.com/license
+ *
+ * @package     Fibber
+ * @link        https://fibberpackage.com
+ * @copyright   Copyright (c) 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://fibberpackage.com/license
+ */
+
 namespace Jalexcam\Fibber\Container;
 
 class ContainerMixin
