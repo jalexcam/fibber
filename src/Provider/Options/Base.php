2288bd0ecdc42cfc44f8a0fce1e4c3c0cd6cd504
Alexander Campo <jalexcam@gmail.com>
2026-06-26 22:34:58 -0500

    Formatting

--- src/Provider/Options/Base.php
+++ src/Provider/Options/Base.php
@@ -18,31 +18,26 @@
 
 namespace Fibber\Provider\Options;
 
-use Random\Randomizer;
 use ReflectionClass;
+use Fibber\Generator;
 
 /**
  * Allows generate a random of element from the given an array.
  */
 class Base
 {
-    /**
-     * Get the random of data.
-     * 
-     * @var Randomizer
-     */
-    protected Randomizer $randomizer;
+    protected Generator $generator;
 
     /**
      * Constructor. Create a new Base instance.
      * 
-     * @param Randomizer $randomizer
+     * @param Generator $generator
      * 
      * @return void
      */
-    public function __construct(Randomizer $randomizer)
+    public function __construct(Generator $generator)
     {
-        $this->randomizer = $randomizer;
+        $this->generator = $generator;
     }
 
     /**
@@ -60,88 +55,5 @@ class Base
                     (new ReflectionClass($this))->getShortName()
                 )
             );
-    }
-
-    /**
-     * Return a given number of random elements from the given array.
-     *
-     * @param array $array
-     * @param int $elements
-     *
-     * @return array
-     */
-    protected function getArrayRandomElements(array $array, int $elements = 1): array
-    {
-        $keys = $this->randomizer->pickArrayKeys($array, $elements);
-
-        return array_intersect_key($array, array_flip($keys));
-    }
-
-    /**
-     * Return a random element from the given array.
-     *
-     * @param array $array
-     *
-     * @return mixed
-     */
-    protected function getArrayRandomElement(array $array): mixed
-    {
-        $elements = $this->getArrayRandomElements($array);
-
-        return reset($elements);
-    }
-
-    /**
-     * Return a random keys from the given array.
-     * 
-     * @param array $array
-     * @param int $elements
-     * 
-     * @return array
-     */
-    protected function getArrayRandomKeys(array $array, int $elements = 1): array
-    {
-        return $this->randomizer->pickArrayKeys($array, $elements);
-    }
-
-    /**
-     * Return a number of first key random from the given array.
-     * 
-     * @param array $array
-     * 
-     * @return mixed
-     */
-    protected function getArrayRandomKey(array $array): mixed
-    {
-        return $this->getArrayRandomKeys($array)[0];
-    }
-
-    /**
-     * Get the format string.
-     * 
-     * @param string $string
-     * 
-     * @return string
-     */
-    protected function formatString(string $string): string
-    {
-        while (($pos = strpos($string, '{a}')) !== false) {
-            $string = substr_replace($string, $this->getArrayRandomElement(['{d}', '{l}']), $pos, 3);
-        }
-
-        while (($pos = strpos($string, '{d}')) !== false) {
-            $string = substr_replace($string, (string) $this->randomizer->getInt(0, 9), $pos, 3);
-        }
-
-        while (($pos = strpos($string, '{l}')) !== false) {
-            $string = substr_replace(
-                $string, 
-                $this->randomizer->getBytesFromString(implode(range('A', 'Z')), 1), 
-                $pos, 
-                3
-            );
-        }
-
-        return $string;
     }
 }
