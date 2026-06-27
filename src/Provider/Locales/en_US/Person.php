615d901a451898a45d162e5c37d760c3b7f5863d
Alexander Campo <jalexcam@gmail.com>
2026-06-27 00:02:26 -0500

    [1.x] Added new class

---
+++ src/Provider/Locales/en_US/Person.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Fibber Package
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://fibberpackage.com/license
+ *
+ * @package     Fibber
+ * @link        https://fibberpackage.com
+ * @copyright   Copyright (c) 2026 Alexander Campo <jalexcam@gmail.com>
+ * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://fibberpackage.com/license
+ */
+
+namespace Fibber\Provider\Locales\en_US;
+
+use Fibber\Provider\Options\Person as BasePerson;
+
+class Person extends BasePerson
+{
+
+}
